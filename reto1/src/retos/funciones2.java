15c3a00f209a77e54c9a9b3e6289bad31ebc14f5
Alice Jamir <mariaalicejamir@gmail.com>
2024-10-29 09:49:35 +0100

    Creacion de las funciones esPositivo y esNegativo

--- reto1/src/retos/funciones2.java
+++ reto1/src/retos/funciones2.java
@@ -2,16 +2,30 @@ package retos;
 
 public class funciones2 {
 
-	//(multiplicar, dividir) y de los métodos (esPositivo y esNegativo)
+	// (multiplicar, dividir) y de los métodos (esPositivo y esNegativo)
 	public static int multiplicar(int n1, int n2) {
-		int n3= n1 * n2;
-		return n3;	
+		int n3 = n1 * n2;
+		return n3;
 	}
-	
+
 	public static int dividir(int n1, int n2) {
-		int n3= n1 / n2;
-		return n3;	
+		int n3 = n1 / n2;
+		return n3;
 	}
-	
-	
+
+	public static void esPositivo(int n1) {
+
+		if (n1 > 0) {
+			System.out.println("Positivo");
+		}
+
+	}
+
+	public static void esNegatico(int n1) {
+
+		if (n1 < 0) {
+			System.out.println("Negativo");
+		}
+	}
+
 }
