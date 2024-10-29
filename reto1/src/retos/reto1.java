614a3291e4320f5f2e70fbf0d21c309d56c06fe1
Vixx-gs <victorgallegoserrate@gmail.com>
2024-10-29 11:20:32 +0100

    Todo hecho

--- reto1/src/retos/reto1.java
+++ reto1/src/retos/reto1.java
@@ -56,8 +56,69 @@ public class reto1 {
 					System.out.println("Introduce un numero ");
 					String s1 = sc.nextLine();
 					n1=Integer.parseInt(s1);
+					boolean par = funciones1.par(n1);
+					System.out.println(par);
+				}
+				if(n==4) {
+					System.out.println("Introduce un numero ");
+					String s1 = sc.nextLine();
+					n1=Integer.parseInt(s1);
+					boolean impar= funciones1.impar(n1);
+					System.out.println(impar);
+				}
+				if(n==5) {
+					System.out.println("introduce 2 numeros ");
+					String s1 = sc.nextLine();
+					n1=Integer.parseInt(s1);
+					String s2= sc.nextLine();
+					n2=Integer.parseInt(s2);
+					int multi= funciones2.multiplicar(n1, n2);
+					System.out.println(multi);
+				}
+				if(n==6) {
+					System.out.println("introduce 2 numeros ");
+					String s1 = sc.nextLine();
+					n1=Integer.parseInt(s1);
+					String s2= sc.nextLine();
+					n2=Integer.parseInt(s2);
+					int div= funciones2.dividir(n1, n2);
+					System.out.println(div);
+				}
+				if(n==7) {
+					System.out.println("Introduce un numero ");
+					String s1 = sc.nextLine();
+					n1=Integer.parseInt(s1);
+					boolean positivo= funciones2.esPositivo(n1);
+					System.out.println(positivo);
 					
-					
+				}
+				if(n==8){
+					System.out.println("Introduce un numero ");
+					String s1 = sc.nextLine();
+					n1=Integer.parseInt(s1);
+					boolean nega= funciones2.esNegatico(n1);
+					System.out.println(nega);
+				}
+				if(n==9) {
+					System.out.println("Introduce un numero ");
+					String s1 = sc.nextLine();
+					n1=Integer.parseInt(s1);
+					boolean multiplo7= Funciones3.multiplo7(n1);
+					System.out.println(multiplo7);
+				}
+				if(n==10) {
+					System.out.println("Introduce un numero ");
+					String s1 = sc.nextLine();
+					n1=Integer.parseInt(s1);
+					int divi= Funciones3.divisormayor1(n1);
+					System.out.println(divi);
+				}
+				if(n==11) {
+					System.out.println("Introduce un numero ");
+					String s1 = sc.nextLine();
+					n1=Integer.parseInt(s1);
+					boolean may20= Funciones3.parmayor20(n1);
+					System.out.println(may20);
 				}
 			}
 			catch(Exception ex)
