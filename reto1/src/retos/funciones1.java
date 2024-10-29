f26ec2cb3a93fd5faffa7f614dbcfaa31280e7bb
Vixx-gs <victorgallegoserrate@gmail.com>
2024-10-29 09:45:26 +0100

    Creados los 4 por Victor

--- reto1/src/retos/funciones1.java
+++ reto1/src/retos/funciones1.java
@@ -1,5 +1,24 @@
 package retos;
 
 public class funciones1 {
-
+	public static int sumar(int a, int b) {
+		int c;
+		c= a + b;
+		return c;
+	}
+	public static int restar(int a, int b) {
+		int c;
+		c=a-b;
+		return c;
+	}
+	public static void par(int a) {
+		if(a%2==0) {
+			System.out.println("Es par");
+		}
+	}
+	public static void impar(int a) {
+		if(a%2!=0) {
+			System.out.println("Es impar");
+		}
+	}
 }
