a721bda2c03adc7878b7efb54a279fafcc710ecc
Vixx-gs <victorgallegoserrate@gmail.com>
2024-10-29 10:19:27 +0100

    Haciendo el menu

--- reto1/src/retos/reto1.java
+++ reto1/src/retos/reto1.java
@@ -1,9 +1,27 @@
 package retos;
 
+import java.util.Scanner;
+
 public class reto1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n=0;
+		do
+		{
+			try
+			{
+				System.out.println("Introduce un nº");
+				String s = sc.nextLine();
+				n=Integer.parseInt(s);
+				break;
+			}
+			catch(Exception ex)
+			{
+				System.out.println("Introduce un nº correcto");
+			}
+		}while(true);
+
 
 	}
 
