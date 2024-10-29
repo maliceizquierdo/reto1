d48a12ada01a3895bd7ac212c32b55d649f99eea
Alice Jamir <mariaalicejamir@gmail.com>
2024-10-29 09:58:04 +0100

    Hecho las 4

--- reto1/src/retos/funciones2.java
+++ reto1/src/retos/funciones2.java
@@ -2,14 +2,17 @@ package retos;
 
 public class funciones2 {
 
-	// (multiplicar, dividir) y de los métodos (esPositivo y esNegativo)
 	public static int multiplicar(int n1, int n2) {
 		int n3 = n1 * n2;
 		return n3;
 	}
 
 	public static int dividir(int n1, int n2) {
+		
 		int n3 = n1 / n2;
+		if(n2==0) {
+			return 0;
+		}
 		return n3;
 	}
 
