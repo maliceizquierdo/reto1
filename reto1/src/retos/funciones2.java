2c5021b14a38ec97067c7282d441b08eee35b364
Alice Jamir <mariaalicejamir@gmail.com>
2024-10-29 09:46:48 +0100

    Creacion de las funciones de multiplicar y dividir

--- reto1/src/retos/funciones2.java
+++ reto1/src/retos/funciones2.java
@@ -2,4 +2,16 @@ package retos;
 
 public class funciones2 {
 
+	//(multiplicar, dividir) y de los métodos (esPositivo y esNegativo)
+	public static int multiplicar(int n1, int n2) {
+		int n3= n1 * n2;
+		return n3;	
+	}
+	
+	public static int dividir(int n1, int n2) {
+		int n3= n1 / n2;
+		return n3;	
+	}
+	
+	
 }
